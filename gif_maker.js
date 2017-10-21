f5f8346f2af00005356a18426ad43e31c098528f
mik4 <mmick@protonmail.com>
2017-10-21 13:53:03 +0200

    add class Utils and Project

--- gif_maker.js
+++ gif_maker.js
@@ -1,9 +1,15 @@
+import { fs } from 'fs';
+import { swal } from 'sweetalert';
+import { _ } from 'underscore';
 
 class GifMaker {
     constructor() {
+        this.project = undefined;
+        this.utils = new Utils();
         GifMaker.fixBehavior();
         this.getDOMElements();
         this.bindEvents();
+        this.baseOutputPath = process.env.PWD + '/output/';
     }
 
     getDOMElements() {
@@ -67,7 +73,23 @@ class GifMaker {
     }
 
     createProjectMethod() {
+        const name = this.utils.slugify(_.trim(this.projectNameInput.value));
+        const path = this.baseOutputPath + name;
 
+        fs.exists(path, (exist) => {
+            if (exist) {
+                this.utils.notify('Un projet du même nom existe déjà', 'error');
+                return;
+            }
+
+            fs.mkdir(path, () => {
+                this.project = new Project(this.baseOutputPath, name);
+                this.createButton.className = 'hidden';
+                this.deleteProjectButton.className = '';
+                this.utils.notify('ok', 'success');
+                Utils.scroll(this.uploadPage);
+            });
+        });
     }
 
     deleteProjectMethod() {
@@ -136,5 +158,72 @@ class GifMaker {
 
     quitMethod() {
 
+    }
+}
+
+class Utils {
+    static scroll(target) {
+        return window.scrollTo(0, target.offsetTop);
+    }
+
+    slugify(s) {
+        let str = s;
+        const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
+        const to = 'aaaaeeeeiiiioooouuuunc------';
+
+        str = str.replace(/^\s+|\s+$/g, '');
+        str = str.toLowerCase();
+
+        _.each(from, (f, i) => {
+            str = str.replace(new RegExp(f, 'g'), to.charAt(i));
+        });
+
+        str = str
+            .replace(/[^a-z0-9 -]/g, '')
+            .replace(/\s+/g, '-')
+            .replace(/-+/g, '-');
+
+        return str;
+    }
+
+    notify(text, type, arg, callback) {
+        const title =
+            ('success' === type)
+                ? 'Bravo'
+                : ('warning' === type)
+                ? 'Attention'
+                : ('error' === type)
+                    ? 'Erreur'
+                    : ('confirm' === type)
+                        ? 'Confirmation'
+                        : 'Message';
+
+        if ('confirm' === type) {
+            return swal({
+                title: title,
+                text: text,
+                type: 'info',
+                showCancelButton: true,
+                confirmButtonText: 'Confirmer',
+                cancelButtonText: 'Annuler',
+                closeOnConfirm: true,
+                closeOnCancel: true,
+            }, (isConfirm) => {
+                if (isConfirm) {
+                    return callback ? callback(arg) : true;
+                }
+            });
+        }
+
+        return swal(title, text, type);
+    }
+}
+
+class Project {
+    constructor(base, name) {
+        this.path = base + name + '/';
+        this.name = name;
+        this.pictures = [];
+        this.rank = [];
     }
 }
